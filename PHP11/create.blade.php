0a5d7a18e2c38e9d54027c75ec98011e78cdc71e
”Takehiro” <ponta.engineer@gmail.com>
2019-06-09 07:41:51 +0000

    first

---
+++ PHP11/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.profile')
+@section('title', 'Myプロフィール')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
